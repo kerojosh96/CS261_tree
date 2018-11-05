0023bc667d3e817acd27fd6ec00d61cd5acaf682
kerojosh96 <jaybirdcook@gmail.com>
2018-11-05 13:28:20 -0800

    Create bst.py

---
+++ bst.py
@@ -0,0 +1,14 @@
+class BinarySearchTreeNode:
+    
+    def __init__(self,value = None,left=None,right=None):
+        
+        self.value = value #creates a value and stores it
+        self.left = left
+        self.right = right
+        self.parent = None
+    def getVal(self):
+        return self.value
+   
+
+    
+   
