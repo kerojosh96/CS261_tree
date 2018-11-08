cd511dee22816e6cee433a55355ea8571176a968
kerojosh96 <jaybirdcook@gmail.com>
2018-11-08 11:43:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import unittest
 from bst import BinarySearchTreeNode
 from test_bst import TestBinarySearchTree
+def Create_test():
+    print(" ")
+    print(" ")
+    bb = TestBinarySearchTree()
+    bb.test_insert_Call(50)
+    bb.test_insert_Call(6)
+    bb.test_insert_Call(56)
+    bb.test_insert_Call(23)
+    bb.test_insert_Call(2)
+    bb.test_insert_Call(200)
+    bb.test_insert_Call(59)
+    bb.test_insert_Call(125)
+    bb.test_insert_Call(126)
+    cc = TestBinarySearchTree()
+    cc.test_insert_Call(200)
+    cc.test_insert_Call(210)
+    cc.test_insert_Call(190)
 
-bb = TestBinarySearchTree()
-bb.test_insert_Root(50)
-bb.test_insert_Root(6)
-bb.test_insert_Root(56)
-bb.test_insert_Root(23)
-bb.test_insert_Root(2)
-bb.test_insert_Root(200)
-bb.test_insert_Root(59)
-bb.test_insert_Root(125)
-bb.test_insert_Root(126)
-#bb.callInorder()
-print("calling Post Order")
-bb.callPostorder()
-print("calling In Order")
-bb.callInorder()
-print(str(bb.NumberOfNodes)+" number of nodes" )
-bb.call_find(56)
-bb.call_find(59)
-
-#bb.call_find(46)
-#bb.call_deletion(200)
+    cc.call_deletion(190) #deletes leaf
+    cc.call_deletion(210) #deletes leaf
+    print("calling Post Order")
+    print(" ")
+    bb.callPostorder()
+    print("calling In Order")
+    bb.callInorder()
+    print(str(bb.NumberOfNodes)+" number of nodes" )
+    bb.call_find(56)
+   
+    
+Create_test()
